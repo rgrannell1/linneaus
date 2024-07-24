fc5943f80b529b14601ff56eb4426a95ef0ee172
Róisín Grannell <r.grannell2@gmail.com>
2024-07-24 22:28:26 +0100

    updated nav links

--- static/js/view/navigation-guide.js
+++ static/js/view/navigation-guide.js
@@ -3,8 +3,9 @@ import { html } from "/js/library/lit.js";
 
 export class LinnaeusNavigationGuide extends LitElem {
   render() {
+    // I don't like the term "content", but hey it's standard now.
     return html`
-      <p class="navigation-guide">Navigate between photos with ←, →</p>
+      <p class="navigation-guide">Navigate between content with ←, →</p>
       <p class="navigation-guide">Navigate between questions with ↑, ↓</p>
     `;
   }
